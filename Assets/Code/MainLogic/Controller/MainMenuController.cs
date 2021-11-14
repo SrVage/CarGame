88c18390fc4c112c65ec83b126a66b2aaf554d1f
Krampit Maksim <75555341+SrVage@users.noreply.github.com>
2021-11-14 17:51:28 +0700

    NotificationAndLocalization

--- Assets/Code/MainLogic/Controller/MainMenuController.cs
+++ Assets/Code/MainLogic/Controller/MainMenuController.cs
@@ -1,12 +1,16 @@
-﻿using Code.MainLogic.Model;
+﻿using System;
+using Code.MainLogic.Model;
 using Code.MainLogic.View;
 using Code.Tools;
+using Unity.Notifications.Android;
+using Unity.Notifications.iOS;
 using UnityEngine;
 
 namespace Code.MainLogic.Controller
 {
     public class MainMenuController:BaseController
     {
+        private const string AndroidNotifierId = "android_notifier_id";
         private ResourcePath _mainMenuResourcePath = new ResourcePath() { PathResource = "Prefabs/mainMenu"};
         private ProfilePlayer _model;
         private MainMenuView _view;
@@ -17,6 +21,7 @@ namespace Code.MainLogic.Controller
             AddGameObject(_view.gameObject);
             _view.Init(OnStart, Reward, Fight);
             _model = model;
+            CreateNotification();
         }
 
         private void OnStart()
@@ -41,5 +46,34 @@ namespace Code.MainLogic.Controller
             var view = go.GetComponent<MainMenuView>();
             return view;
         }
+        
+        private void CreateNotification()
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                var androidSettingsChanel = new AndroidNotificationChannel
+                {
+                    Id = AndroidNotifierId,
+                    Name = "Car Game",
+                    Importance = Importance.High,
+                    CanBypassDnd = true,
+                    CanShowBadge = true,
+                    Description = "You entered game for a long time",
+                    EnableLights = true,
+                    EnableVibration = true,
+                    LockScreenVisibility = LockScreenVisibility.Public
+                };
+
+                AndroidNotificationCenter.RegisterNotificationChannel(androidSettingsChanel);
+
+                var androidSettingsNotification = new AndroidNotification
+                {
+                    Color = Color.white,
+                    FireTime = DateTime.Today.Date.AddDays(1)
+                };
+                AndroidNotificationCenter.SendNotification(androidSettingsNotification, AndroidNotifierId);
+            }
+        }
+
     }
 }
---
+++ Assets/Code/MainLogic/View/Localization.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
+
+namespace Code.MainLogic.View
+{
+    public class Localization:MonoBehaviour
+    {
+        [SerializeField]
+        private Button _englishButton;
+  
+        [SerializeField]
+        private Button _russianButton;
+
+        [SerializeField] private Text _start;
+        [SerializeField] private Text _fight;
+        [SerializeField] private Text _reward;
+
+        private void Start(){
+            ChangedLocaleEvent(null);
+            LocalizationSettings.SelectedLocaleChanged += ChangedLocaleEvent;
+      
+            _russianButton.onClick.AddListener(() => ChangeLanguage(1));
+            _englishButton.onClick.AddListener(() => ChangeLanguage(0));
+        }
+
+        private void OnDestroy()
+        {
+            _russianButton.onClick.RemoveAllListeners();
+            _englishButton.onClick.RemoveAllListeners();
+        }
+
+        private void ChangedLocaleEvent(Locale locale)
+        {
+            StartCoroutine(OnChangedLocale(locale));
+        }
+
+        private IEnumerator OnChangedLocale(Locale locale)
+        {
+            var loadingOperation = LocalizationSettings.StringDatabase.GetTableAsync("New Table");
+            yield return loadingOperation;
+       
+            if (loadingOperation.Status == AsyncOperationStatus.Succeeded)
+            {
+                var table = loadingOperation.Result;
+                _start.text = table.GetEntry("StartGame")?.GetLocalizedString();
+                _fight.text = table.GetEntry("Fight")?.GetLocalizedString();
+                _reward.text = table.GetEntry("Reward")?.GetLocalizedString();
+                
+            }
+            else
+            {
+                Debug.Log("Could not load String Table\n" + loadingOperation.OperationException);
+            }
+        }
+
+        private void ChangeLanguage(int index)
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+        }
+    }
+}
